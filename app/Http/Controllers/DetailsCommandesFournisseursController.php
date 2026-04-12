5be94b4704fb51d7957ee21ea2f3dc626e28bbe2
Anas Lagziri <anasl1gziri@gmail.com>
2026-04-12 23:13:10 +0200

    add details_commandes_fournisseurs model , controller

---
+++ app/Http/Controllers/DetailsCommandesFournisseursController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\detailsCommandesFournisseurs;
+use Illuminate\Http\Request;
+
+class DetailsCommandesFournisseursController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(detailsCommandesFournisseurs $detailsCommandesFournisseurs)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(detailsCommandesFournisseurs $detailsCommandesFournisseurs)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, detailsCommandesFournisseurs $detailsCommandesFournisseurs)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(detailsCommandesFournisseurs $detailsCommandesFournisseurs)
+    {
+        //
+    }
+}
---
+++ app/Models/detailsCommandesFournisseurs.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class detailsCommandesFournisseurs extends Model
+{
+    protected $table = 'details_commandes_fournisseurs';
+    protected $fillable = ['id','commande_fournisseur_id','produit_id','quantite','prix_unitaire','prix_total'];
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    public $timestamps = false;
+    public function fournisseurs(){
+        return $this->hasMany(commandesFournisseurs::class, 'id' , 'commande_fournisseur_id' );
+    }
+    public function produits(){
+        return $this->hasMany(Produits::class , 'id' , 'produit_id'); 
+    }
+}
